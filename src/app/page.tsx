6ac00ad8a1a3c863c55e6642b366765b18030028
Gabriel <gabrielleitemoreira@gmail.com>
2025-05-16 01:20:05 -0300

    fixeding the default page on access the site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
+// app/page.tsx
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="text-center py-20">
-
-
-    </div>
-  );
+  redirect("/noticias");
 }
